712270cd8398d8e28ba96d446312ca74158bda39
Sören Jensen <sirn@sirn.se>
2023-04-14 15:41:43 +0200

    Version 1.0

---
+++ tests/LoggerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phrity\Net\Mock\Test;
+
+use PHPUnit\Framework\TestCase;
+use Phrity\Net\Mock\{
+    EchoLogger,
+    Mock,
+    StreamFactory,
+};
+use Psr\Log\{
+    LoggerInterface,
+    NullLogger
+};
+
+/**
+ * Phrity\Net\Mock\Test\LoggerTest test class.
+ */
+class LoggerTest extends TestCase
+{
+    public function testLogger(): void
+    {
+        $logger = new EchoLogger();
+
+        ob_start();
+        $logger->warning("A {level} message.", [
+            'level' => 'warning',
+            'object' => (object)['a' => 'b'],
+            'array' => [1]
+        ]);
+        $out = ob_get_clean();
+        $this->assertEquals(
+            "[warning] A warning message. {\"level\":\"warning\",\"object\":\"stdClass\",\"array\":\"array\"}\n",
+            $out
+        );
+    }
+
+    public function testMoggerLogger(): void
+    {
+        Mock::setLogger(new EchoLogger());
+
+        ob_start();
+        $factory = new StreamFactory();
+        $out = ob_get_clean();
+        $this->assertEquals("[debug] StreamFactory.__construct {}\n", $out);
+
+        Mock::setLogger(new NullLogger());
+    }
+}
